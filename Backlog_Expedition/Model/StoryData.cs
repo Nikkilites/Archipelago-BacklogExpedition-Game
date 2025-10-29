99787cfa91472989a48922985a69afac157d2467
Nikkilite <Niko6697n@gmail.com>
2025-10-29 19:27:12 +0100

    added stuff to StoryData

--- Backlog_Expedition/Model/StoryData.cs
+++ Backlog_Expedition/Model/StoryData.cs
@@ -5,6 +5,8 @@
         public List<string> login = login;
         public List<string> introduction = introduction;
         public List<string> goal = goal;
-        public Dictionary<string,List<string>> treasure_descriptions = treasure_descriptions;
+        public Dictionary<string, List<string>> open_chest = treasure_descriptions;
+        public Dictionary<string, List<string>> slay_monster = treasure_descriptions;
+        public Dictionary<string, List<string>> treasure_descriptions = treasure_descriptions;
     }
 }
