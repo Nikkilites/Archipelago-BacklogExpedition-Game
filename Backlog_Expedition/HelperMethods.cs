c4ed36688928877628c67c55b3cceb0ecad07ab1
Nikkilite <Niko6697n@gmail.com>
2025-10-30 12:45:22 +0100

    Remove unwanted helper method

--- Backlog_Expedition/HelperMethods.cs
+++ Backlog_Expedition/HelperMethods.cs
@@ -1,23 +1,7 @@
-﻿using Newtonsoft.Json;
-
-namespace Backlog_Expedition
+﻿namespace Backlog_Expedition
 {
     public static class HelperMethods
     {
-        public static object LoadJson<T>(string filepath)
-        {
-            if (!File.Exists(filepath))
-            {
-                throw new Exception($"File {filepath} not found.");
-            }
-
-            Log($"Will load data from {filepath}");
-            string json = File.ReadAllText(filepath);
-            T data = JsonConvert.DeserializeObject<T>(json);
-            Log($"Data {data} loaded from {filepath}");
-            return data;
-        }
-
         private static readonly object logLock = new();
 
         public static void Log(string logText)
