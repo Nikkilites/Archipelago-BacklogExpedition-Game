a466d23354bdf6c0e5212cbd1c096a7cc2088c06
Nikkilite <Niko6697n@gmail.com>
2026-05-28 19:14:07 +0200

    Only send goal if goal had just been found. Also return whether it was sent or not

--- BEx.Core/GoalHandler.cs
+++ BEx.Core/GoalHandler.cs
@@ -7,14 +7,24 @@
         public int TreasuresToGoal { get; set; } = 0;
         public int TreasuresFound => _gameSession.RegionHandler.Regions.Count(r => r.TreasureFound);
         public event Action? GoalReached;
-        public void CheckIfGoal()
+        private bool _goalHasBeenSent = false;
+        public bool CheckIfGoal()
         {
-            if (TreasuresFound == TreasuresToGoal)
+            if (TreasuresFound == TreasuresToGoal & !_goalHasBeenSent)
+            {
                 OnGoalConditionMet();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         private void OnGoalConditionMet()
         {
+            _goalHasBeenSent = true;
+
             _gameSession.Logger.Log($"{_gameSession.ConnectionHandler.PlayerName} reached goal!");
 
             _gameSession.ConnectionHandler.SendGoal();
